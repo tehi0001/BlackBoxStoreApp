ff1d4e1c0c9eb5d8e965d1ac047b2b8135e8a10c
Timothy Ehimen <tehi0001@student.monash.edu>
2021-08-05 14:38:26 +0200

    Added main header and footer components, bootstrapped router

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from "@angular/material/toolbar";
-
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		MatToolbarModule
+		MatToolbarModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatIconModule,
+		MatBadgeModule
 	],
 	exports: [
-		MatToolbarModule
+		MatToolbarModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatIconModule,
+		MatBadgeModule
 	]
 })
 export class MaterialModule {}
